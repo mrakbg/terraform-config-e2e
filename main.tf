e0773eae6e8068cb247eead2b99ee45e93d2935a
Anuj Gupta <anujguptaa1234567@gmail.com>
2025-03-19 18:40:03 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -42,7 +42,7 @@ resource "google_compute_instance" "compute_engine" {
   name         = "e2eyui"
   machine_type = "e2-micro"
   zone         = "us-central1-b"
-  count        = 2
+  count        = 3
 
   network_interface {
     network    = google_compute_network.network.id  # Correct reference
